56eea745f1199b17509af0841f2730dc99653055
alostsock <49344439+alostsock@users.noreply.github.com>
2022-08-10 23:29:07 -0400

    add note about lint-staged and tsc

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,4 +1,6 @@
 export default {
-	"*.{ts,tsx}": [() => "pnpm tsc", "eslint --fix", "prettier --write"],
+	// We need to run tsc with this function syntax so that tsconfig settings are picked up.
+	// See: https://github.com/okonet/lint-staged/issues/825#issuecomment-620018284
+	"*.{ts,tsx}": [() => "tsc", "eslint --fix", "prettier --write"],
 	"*.{html,css,scss,json,md}": ["prettier --write"],
 };
